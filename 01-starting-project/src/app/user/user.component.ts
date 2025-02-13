b8ab5d9b75397d0faed739d44d081880da31d621
Gustavo Gois <gustavo.gois@gmail.com>
2025-02-13 22:50:08 +0000

    section 2 - lesson 26 - Managing State & Changing Data

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -12,10 +12,12 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  onSelectedUser() {
-    console.log('Clicked!');
-  }
   selectedUser = DUMMY_USERS[randomIndex];
+
+  onSelectedUser() {
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser = DUMMY_USERS[randomIndex];
+  }
 
   get imagePath() {
     return 'assets/users/' + this.selectedUser.avatar;
