25cd5018c27bde77d0eb85aacfd8a2e1aef3e7cd
Gustavo Gois <gustavo.gois@gmail.com>
2025-02-14 11:51:15 +0000

    section 2 - lesson 30 - Defining Component Inputs

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 import { DUMMY_USERS } from '../dummy-users';
 
@@ -12,14 +12,14 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
-
-  onSelectedUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser = DUMMY_USERS[randomIndex];
-  }
+  @Input() avatar!: string;
+  @Input() name!: string;
 
   get imagePath() {
-    return 'assets/users/' + this.selectedUser.avatar;
+    return 'assets/users/' + this.avatar;
+  }
+
+  onSelectUser() {
+    console.log('User selected!');
   }
 }
