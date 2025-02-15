50fd631a8672b87ac5362ea3963d4d5110fb9f69
Gustavo Gois <gustavo.gois@gmail.com>
2025-02-15 21:41:59 +0000

    Seção 2 - Lesson 34 - Working with Outputs & Emitting Data

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,6 +1,6 @@
-import { Component, Input } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 
-import { DUMMY_USERS } from '../dummy-users';
+import {DUMMY_USERS} from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
@@ -12,14 +12,17 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
+
+  @Input({required: true}) id!: string;
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
+  @Output() select = new EventEmitter();
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
   }
 
   onSelectUser() {
-    console.log('User selected!');
+    this.select.emit(this.id);
   }
 }
