ff69b329b6d5cbad76aa29deab93f810e47d1c2b
Gustavo Gois <gustavo.gois@gmail.com>
2025-02-15 10:48:08 +0000

    with signals

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, computed, Input, input } from '@angular/core';
 
 import { DUMMY_USERS } from '../dummy-users';
 
@@ -12,12 +12,16 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
+  //@Input({ required: true }) avatar!: string;
+  //@Input({ required: true }) name!: string;
+  avatar = input.required<string>();
+  name = input.required<string>();
 
-  get imagePath() {
+  imagePath = computed(() => 'assets/users/' + this.avatar);
+
+  /*get imagePath() {
     return 'assets/users/' + this.avatar;
-  }
+  }*/
 
   onSelectUser() {
     console.log('User selected!');
