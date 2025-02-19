504a4f0dd8b27be32c7b66a37d81a10198057f22
Gustavo Gois <gustavo.gois@gmail.com>
2025-02-19 15:51:25 +0000

    Section 2- lesson 39 - Accepting Objects As Inputs & Adding Appropriate Typings

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -13,16 +13,18 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 })
 export class UserComponent {
 
-  @Input({required: true}) id!: string;
-  @Input({required: true}) avatar!: string;
-  @Input({required: true}) name!: string;
+  @Input({required: true}) user!: {
+    id: string,
+    name: string,
+    avatar: string
+  };
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
