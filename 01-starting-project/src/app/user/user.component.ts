4e5a8a2730f14ad900aa23da60d323e10e20a6b6
Gustavo Gois <gustavo.gois@gmail.com>
2025-02-21 12:07:48 +0000

    Section 2- lesson 47 - Storing Data Models in Separate Files

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,20 +1,5 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
-
-import {DUMMY_USERS} from '../dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-
-/*type User = {
-  id: string,
-  name: string,
-  avatar: string
-}*/
-
-interface User {
-  id: string,
-  name: string,
-  avatar: string
-}
+import {type User} from "./user.model";
 
 @Component({
   selector: 'app-user',
