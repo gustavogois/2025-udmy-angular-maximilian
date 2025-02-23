1a03f91927878859044d3ba27f2ca67143d264be
Gustavo Gois <gustavo.gois@gmail.com>
2025-02-23 18:54:38 +0000

    Section 2- lesson 61 - More service usage instead of Emitter

--- 01-starting-project/src/app/tasks/task/task.component.ts
+++ 01-starting-project/src/app/tasks/task/task.component.ts
@@ -2,6 +2,7 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {type Task} from "./task.model";
 import {CardComponent} from "../../shared/card/card.component";
 import {DatePipe} from "@angular/common";
+import {TasksService} from "../tasks.service";
 
 @Component({
   selector: 'app-task',
@@ -15,9 +16,12 @@ import {DatePipe} from "@angular/common";
 })
 export class TaskComponent {
   @Input({required: true}) task!: Task;
-  @Output() complete = new EventEmitter<string>();
+
+  constructor(private readonly tasksService: TasksService) {
+    this.tasksService = tasksService;
+  }
 
   onCompleteTask() {
-    this.complete.emit(this.task.id);
+    this.tasksService.removeTask(this.task.id);
   }
 }
