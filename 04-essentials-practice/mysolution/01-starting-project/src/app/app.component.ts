78c3f2c75a0ed5c6892d3aaa9b0036d78a359dfd
Gustavo Gois <gustavo.gois@gmail.com>
2025-02-24 17:05:06 +0000

    04-essentials-practice - Result without signals and without services

--- 04-essentials-practice/mysolution/01-starting-project/src/app/app.component.ts
+++ 04-essentials-practice/mysolution/01-starting-project/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {HeaderComponent} from "./header/header.component";
 import {UserInputComponent} from "./user-input/user-input.component";
 import { type InvestmentInput} from "./investment-input.model";
+import {InvestmentResultsComponent} from "./investment-results/investment-results.component";
 
 @Component({
   selector: 'app-root',
@@ -9,10 +10,20 @@ import { type InvestmentInput} from "./investment-input.model";
   templateUrl: './app.component.html',
   imports: [
     HeaderComponent,
-    UserInputComponent
+    UserInputComponent,
+    InvestmentResultsComponent
   ]
 })
 export class AppComponent {
+  resultsData?: {
+    year: number;
+    interest: number;
+    valueEndOfYear: number;
+    annualInvestment: number;
+    totalInterest: number;
+    totalAmountInvested: number;
+  }[];
+
   onCalculateInvestmentResults(data: InvestmentInput) {
     const { initialInvestment, annualInvestment, expectedReturn, duration } =
       data;
@@ -35,7 +46,7 @@ export class AppComponent {
       });
     }
 
-    console.log(annualData);
+    this.resultsData = annualData;
   }
 
 }
