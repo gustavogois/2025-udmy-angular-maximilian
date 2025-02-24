adda0456bdbcb7eb9c20aa6c84ab08da46fe2cbd
Gustavo Gois <gustavo.gois@gmail.com>
2025-02-24 19:25:12 +0000

    04-essentials-practice - final with services

--- 04-essentials-practice/mysolution/01-starting-project/src/app/user-input/user-input.component.ts
+++ 04-essentials-practice/mysolution/01-starting-project/src/app/user-input/user-input.component.ts
@@ -1,6 +1,7 @@
 import {Component, EventEmitter, Output} from '@angular/core';
 import {FormsModule} from "@angular/forms";
 import {type InvestmentInput} from "../investment-input.model";
+import {InvestmentService} from "../investment.service";
 
 @Component({
   selector: 'app-user-input',
@@ -13,20 +14,20 @@ import {type InvestmentInput} from "../investment-input.model";
 })
 export class UserInputComponent {
 
-  @Output() calculate = new EventEmitter<InvestmentInput>();
-
   enteredInitialInvestment = '0';
   enteredAnnualInvestment = '0';
   enteredExpectedReturn = '5';
   enteredDuration = '10';
 
+  constructor(private investmentService: InvestmentService) {}
+
   onSubmit() {
-    this.calculate.emit({
+    this.investmentService.calculateInvestmentResults({
       initialInvestment: +this.enteredInitialInvestment,
       duration: +this.enteredDuration,
       expectedReturn: +this.enteredExpectedReturn,
       annualInvestment: +this.enteredAnnualInvestment
-    });
+    })
     this.enteredDuration = '10';
     this.enteredInitialInvestment = '0';
     this.enteredExpectedReturn = '5';
